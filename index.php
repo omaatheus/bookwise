839336a3a591dda9e5bf76fc3974f41215f90eb4
Matheuss <matheusgida@outlook.com>
2025-10-15 21:40:11 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Book Wise</title>
+</head>
+<body class="bg-stone-950 text-stone-200">
+
+    <header class="bg-stone-900"> 
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl tracking-wide">BookWise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-emerald-600">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li><a class="hover:underline" href="/login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="">
+
+    </main>
+    
+</body>
+</html>
