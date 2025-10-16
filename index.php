059a584efaa47e1301a2e98fe9422507ad6796d7
Matheuss <matheusgida@outlook.com>
2025-10-15 22:38:17 -0300

    feat: added books list

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Book Wise</title>
 </head>
+
 <body class="bg-stone-950 text-stone-200">
 
-    <header class="bg-stone-900"> 
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+    <header class="bg-stone-900">
+        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
             <div class="font-bold text-xl tracking-wide">BookWise</div>
             <ul class="flex space-x-4 font-bold">
                 <li><a href="/" class="text-emerald-600">Explorar</a></li>
@@ -21,9 +23,88 @@
         </nav>
     </header>
 
-    <main class="">
+    <main class="mx-auto max-w-screen-lg space-y-6">
+        <h1 class="mt-6 font-bold text-lg">Explorar</h1>
+
+        <div>
+            <form class="w-full flex space-x-2 mt-6">
+                <input type="text"
+                    class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"
+                    placeholder="Pesquisar" name="search" />
+                <button type="submit">Ir</button>
+            </form>
+        </div>
+
+        <section class="grid-cols-4 grid gap-4">
+            <div class="p-2 rounded border-stone-800 border-2">
+                <div class="flex">
+                    <div class="w-1/3">Imagens</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">3 Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descricao
+                </div>
+            </div>
+                        <div class="p-2 rounded border-stone-800 border-2">
+                <div class="flex">
+                    <div class="w-1/3">Imagens</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">3 Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descricao
+                </div>
+            </div>
+                        <div class="p-2 rounded border-stone-800 border-2">
+                <div class="flex">
+                    <div class="w-1/3">Imagens</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">3 Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descricao
+                </div>
+            </div>
+                        <div class="p-2 rounded border-stone-800 border-2">
+                <div class="flex">
+                    <div class="w-1/3">Imagens</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">3 Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descricao
+                </div>
+            </div>
+                        <div class="p-2 rounded border-stone-800 border-2">
+                <div class="flex">
+                    <div class="w-1/3">Imagens</div>
+                    <div>
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">3 Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descricao
+                </div>
+            </div>
+        </section>
 
     </main>
-    
+
 </body>
+
 </html>
