f437762a4c2ce769042626913969980c4c35070b
Matheus <matheusgida@outlook.com>
2025-10-16 16:40:39 -0300

    feat: views from MVC

---
+++ views/index.view.php
@@ -0,0 +1,37 @@
+
+        <h1 class="mt-6 font-bold text-lg">Explorar</h1>
+        <div>
+            <form class="w-full flex space-x-2 mt-6">
+                <input type="text"
+                    class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"
+                    placeholder="Pesquisar" name="search" />
+                <button type="submit">Ir</button>
+            </form>
+        </div>
+
+       
+<section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+<?php foreach ($livros as $livro): ?>
+    <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+        <div class="flex">
+
+            <div class="w-1/3">Imagem</div>
+
+                <div class="space-y-1">
+
+                    <a href="/livro.php?id=<?=$livro['id'] ?>" class="font-semibold hover:underline"><?=$livro['titulo'] ?></a>
+                    <div class="text-xs italic"><?=$livro['autor'] ?></div>
+                    <div class="text-xs italic"><?=$livro['avaliacao']?></div>
+
+                </div>
+
+            </div>
+
+        <div class="text-sm mt-2"><?=$livro['descricao'] ?></div>
+
+    </div>
+
+<?php endforeach; ?>
+</section>
+            
