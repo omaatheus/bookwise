97d4d5b26891e6072b47a128bf5a4f644592ef8d
Matheuss <matheusgida@outlook.com>
2025-10-15 23:18:47 -0300

    feat: super global variable

---
+++ data.php
@@ -0,0 +1,67 @@
+<?php 
+$livros = [
+    [
+        "id" => 1,
+        "titulo" => "Cartas de um diabo a seu aprendiz",
+        "autor" => "C. S. Lewis",
+        "descricao" => "Uma sátira brilhante em forma de cartas, onde um diabo experiente ensina seu aprendiz sobre como desviar os humanos do bem.",
+        "avaliacao" => "⭐⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 2,
+        "titulo" => "O Hobbit",
+        "autor" => "J. R. R. Tolkien",
+        "descricao" => "A aventura de Bilbo Bolseiro em uma jornada inesperada pelo mundo da Terra-média.",
+        "avaliacao" => "⭐⭐⭐⭐⭐"
+    ],
+    [
+        "id" => 3,
+        "titulo" => "1984",
+        "autor" => "George Orwell",
+        "descricao" => "Um clássico distópico que explora um futuro totalitário onde o governo controla todos os aspectos da vida.",
+        "avaliacao" => "⭐⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 4,
+        "titulo" => "A Revolução dos Bichos",
+        "autor" => "George Orwell",
+        "descricao" => "Uma fábula política que critica regimes autoritários por meio da história de animais que tomam uma fazenda.",
+        "avaliacao" => "⭐⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 5,
+        "titulo" => "O Pequeno Príncipe",
+        "autor" => "Antoine de Saint-Exupéry",
+        "descricao" => "Um conto poético e filosófico sobre amor, amizade e a essência da vida.",
+        "avaliacao" => "⭐⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 6,
+        "titulo" => "Dom Casmurro",
+        "autor" => "Machado de Assis",
+        "descricao" => "Um clássico da literatura brasileira sobre ciúme, memória e ambiguidade, narrado por Bentinho.",
+        "avaliacao" => "⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 7,
+        "titulo" => "A Culpa é das Estrelas",
+        "autor" => "John Green",
+        "descricao" => "Uma emocionante história de amor entre dois adolescentes que enfrentam o câncer.",
+        "avaliacao" => "⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 8,
+        "titulo" => "O Código Da Vinci",
+        "autor" => "Dan Brown",
+        "descricao" => "Um thriller repleto de enigmas envolvendo arte, religião e sociedades secretas.",
+        "avaliacao" => "⭐⭐⭐⭐" 
+    ],
+    [
+        "id" => 9,
+        "titulo" => "A Menina que Roubava Livros",
+        "autor" => "Markus Zusak",
+        "descricao" => "Narrado pela Morte, este romance conta a história de uma menina que encontra consolo nos livros durante a Segunda Guerra Mundial.",
+        "avaliacao" => "⭐⭐⭐⭐⭐" 
+    ]
+];
+?>
--- livro.php
+++ livro.php
@@ -1,3 +1,15 @@
+<?php
+
+require 'data.php';
+
+$id = $_REQUEST['id'];
+
+$filtrado = array_filter($livros, fn($l) =>  $l['id'] == $id);
+
+$livro = array_pop($filtrado);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -23,7 +35,29 @@
         </nav>
     </header>
 
-    <main class="mx-auto max-w-screen-lg space-y-6">
+       <main class="mx-auto max-w-screen-lg space-y-6">
+
+        <?= $livro['titulo']; ?>
+        
+        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+            <div class="flex">
+
+                <div class="w-1/3">Imagem</div>
+
+                <div class="space-y-1">
+
+                    <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+                    <div class="text-xs italic"><?= $livro['autor'] ?></div>
+                    <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
+
+                </div>
+
+            </div>
+
+            <div class="text-sm mt-2"><?= $livro['descricao'] ?></div>
+
+        </div>
 
     </main>
 
