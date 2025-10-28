4874600fed261e9ce283b57e28e241d7e5c0e8f8
Matheus <matheusgida@outlook.com>
2025-10-28 17:43:49 -0300

    feat: Login screen

---
+++ views/login.view.php
@@ -0,0 +1,71 @@
+<div class="mt-6 grid grid-cols-2 gap-2">
+
+    <div class="border border-stone-700 rounded p-4">
+        <h1 class="border-b border-stone-700 rounded font-bold px-4 py-2">Login</h1>
+        <form class= "p-4 space-y-4">
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Email</label>
+                <input 
+                type="email" 
+                name="email" required
+                placeholder="Email"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Senha</label>
+                <input 
+                type="password" 
+                name="password" required
+                placeholder="Senha"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <button type="submit" class="border-stone-800 bg-stone-900 px-3 py-2 rounded-md border-2 font-bold
+            hover:bg-stone-800">Entrar</button>
+        </form>
+
+    </div>
+     <div class="border border-stone-700 rounded p-4">
+        <h1 class="border-b border-stone-700 rounded font-bold px-4 py-2">Registrar</h1>
+        <form class= "p-4 space-y-4">
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Nome</label>
+                <input 
+                type="email" 
+                name="nome" required
+                placeholder="Nome"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Email</label>
+                <input 
+                type="email" 
+                name="email" required
+                placeholder="Email"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Email</label>
+                <input 
+                type="email" 
+                name="email_confirmacao" required
+                placeholder="Confirme seu email"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <div class="flex flex-col"> 
+                <label class="text-stone-400 mb-1">Senha</label>
+                <input 
+                type="password" 
+                name="password" required
+                placeholder="Senha"
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"/>
+            </div>
+            <button type="reset" class="border-stone-800 bg-stone-900 px-3 py-2 rounded-md border-2 font-bold
+            hover:bg-stone-800">Cancelar</button>
+            <button type="submit" class="border-stone-800 bg-stone-900 px-3 py-2 rounded-md border-2 font-bold
+            hover:bg-stone-800">Registrar</button>
+        </form>
+
+    </div>
+    
+
+</div>
